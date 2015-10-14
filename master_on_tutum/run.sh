6c5e2f9e76b20cc5a9e6df5ddd885ebe9539db50
Cagdas Cirit <cagdascirit@gmail.com>
2015-10-15 00:15:52 +0200

    find tutum link nodes and runs test on them

--- master_on_tutum/run.sh
+++ master_on_tutum/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-jmeter.sh -n -Jclient.rmi.localport=60000 -Jclient.tries=3 -Jclient.retries_delay=10000 -Jclient.continue_on_fail=true -Djava.rmi.server.hostname=$HOSTNAME "$@"
+JMETER_SLAVES=$(printenv | grep -o -P '(?<=1099_TCP_ADDR=).*' | awk '{print}' ORS=',')
+
+jmeter.sh -n -Jclient.rmi.localport=60000 -Jclient.tries=3 -Jclient.retries_delay=10000 -Jclient.continue_on_fail=true -Djava.rmi.server.hostname=$HOSTNAME -R $JMETER_SLAVES "$@"
