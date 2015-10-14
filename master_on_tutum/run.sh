3fee7ccc6d790a250582a45c5a3a6100dce0313c
Cagdas Cirit <cagdascirit@gmail.com>
2015-10-15 01:15:05 +0200

    remove duplicate slave IP address

--- master_on_tutum/run.sh
+++ master_on_tutum/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-JMETER_SLAVES=$(printenv | grep -o -P '(?<=1099_TCP_ADDR=).*' | awk '{print}' ORS=',')
+JMETER_SLAVES=$(printenv | grep -o -P '(?<=1099_TCP_ADDR=).*' | awk '!x[$0]++' ORS=',' | awk '{gsub(/,$/,""); print}')
 
 jmeter.sh -n -Jclient.rmi.localport=60000 -Jclient.tries=3 -Jclient.retries_delay=10000 -Jclient.continue_on_fail=true -Djava.rmi.server.hostname=$HOSTNAME -R $JMETER_SLAVES "$@"
