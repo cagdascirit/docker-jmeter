511fb5c813328d907a5d3d10b418b3c071d438c6
unknown <c.cirit@BELLIDPC430.bellid.com>
2015-11-17 12:57:00 +0100

    moved JVM_ARGS from run.sh to Dockerfile

--- slave_running_gatling_on_tutum/run.sh
+++ slave_running_gatling_on_tutum/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-JVM_ARGS="-server -Xss10M -XX:+UseThreadPriorities -XX:ThreadPriorityPolicy=42 -Xms512M -Xmx512M -Xmn100M -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false ${JAVA_OPTS}"
 jmeter.sh -s -Jserver.rmi.localport=60000 -Djava.rmi.server.hostname=$HOSTNAME "$@"
+
